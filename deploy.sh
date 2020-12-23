4a478e2d0175679cc1ced5f93539c1b8fc94f88d
saleem8054 <transparent79twister@gmail.com>
2020-12-23 14:33:41 +0100

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,16 @@
-docker build -t saleem8054/multi-client ./client
-docker build -t saleem8054/multi-server ./server
-docker build -t saleem8054/multi-worker ./worker
+docker build -t saleem8054/multi-client:latest -t saleem8054/multi-client:$SHA ./client
+docker build -t saleem8054/multi-server:latest -t saleem8054/multi-server:$SHA ./server
+docker build -t saleem8054/multi-worker:latest -t saleem8054/multi-worker:$SHA ./worker
 
-docker push saleem8054/multi-client
-docker push saleem8054/multi-server
-docker push saleem8054/multi-worker
+docker push saleem8054/multi-client:latest
+docker push saleem8054/multi-server:latest
+docker push saleem8054/multi-worker:latest
+
+docker push saleem8054/multi-client:$SHA
+docker push saleem8054/multi-server:$SHA
+docker push saleem8054/multi-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=saleem8054/multi-server
+kubectl set image deployments/client-deployment client=saleem8054/multi-client:$SHA
+kubectl set image deployments/server-deployment server=saleem8054/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=saleem8054/multi-worker:$SHA
